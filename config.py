34e2d1bf5888aeed14bf1df976abe72b2959f8ce
grabar.dm <grabar.dm@email.knastu.ru>
2023-10-03 04:23:45 +0300

    change config file

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
+import dataclasses
+
 import yaml
-from typing import ClassVar
+from typing import List
 from dataclasses import dataclass
 
 with open("config.yaml", "r") as __config_yaml:
@@ -9,12 +11,12 @@ with open("config.yaml", "r") as __config_yaml:
 @dataclass
 class ReconstructionsConfig:
     inside_camera_parameters_path: str = _config["reconstruction"]["inside_camera_parameters_path"]
-    thumb_connections: ClassVar[list[list[int, int]]] = _config["reconstruction"]["thumb_connections"]
-    index_connections: ClassVar[list[list[int, int]]] = _config["reconstruction"]["index_connections"]
-    middle_connections: ClassVar[list[list[int, int]]] = _config["reconstruction"]["middle_connections"]
-    ring_connections: ClassVar[list[list[int, int]]] = _config["reconstruction"]["ring_connections"]
-    pinkie_connections: ClassVar[list[list[int, int]]] = _config["reconstruction"]["pinkie_connections"]
-    fingers_colors: ClassVar[list[str]] = _config["reconstruction"]["fingers_colors"]
+    thumb_connections: List[List[int]] = dataclasses.field(default_factory=_config["reconstruction"]["thumb_connections"])
+    index_connections: List[List[int]] = dataclasses.field(default_factory=_config["reconstruction"]["index_connections"])
+    middle_connections: List[List[int]] = dataclasses.field(default_factory=_config["reconstruction"]["middle_connections"])
+    ring_connections: List[List[int]] = dataclasses.field(default_factory=_config["reconstruction"]["ring_connections"])
+    pinkie_connections: List[List[int]] = dataclasses.field(default_factory=_config["reconstruction"]["pinkie_connections"])
+    fingers_colors: List[List[str]] = dataclasses.field(default_factory=_config["reconstruction"]["fingers_colors"])
     world_image: int = _config["reconstruction"]["world_image"]
 
 
@@ -40,7 +42,7 @@ class DetectionConfig:
 
 @dataclass
 class CameraConfig:
-    ids: ClassVar[list[int]] = _config["camera"]["ids"]
+    ids: List[int] = dataclasses.field(default_factory=_config["camera"]["ids"])
     mode: str = _config["camera"]["mode"]
     width: int = _config["camera"]["width"]
     height: int = _config["camera"]["height"]
@@ -48,15 +50,32 @@ class CameraConfig:
 
 @dataclass
 class FingersID:
-    wrist: ClassVar[list[int]] = _config["fingers"]["wrist"]
-    thumb: ClassVar[list[int]] = _config["fingers"]["thumb"]
-    index: ClassVar[list[int]] = _config["fingers"]["index"]
-    middle: ClassVar[list[int]] = _config["fingers"]["middle"]
-    ring: ClassVar[list[int]] = _config["fingers"]["ring"]
-    pinky: ClassVar[list[int]] = _config["fingers"]["pinky"]
+    wrist: List[int] = dataclasses.field(default_factory=_config["fingers"]["wrist"])
+    thumb: List[int] = dataclasses.field(default_factory=_config["fingers"]["thumb"])
+    index: List[int] = dataclasses.field(default_factory=_config["fingers"]["index"])
+    middle: List[int] = dataclasses.field(default_factory=_config["fingers"]["middle"])
+    ring: List[int] = dataclasses.field(default_factory=_config["fingers"]["ring"])
+    pinky: List[int] = dataclasses.field(default_factory=_config["fingers"]["pinky"])
 
 
 @dataclass
 class ArduinoArm:
     com: str = _config["arduino_arm"]["com"]
     baud: int = _config["arduino_arm"]["baud"]
+
+
+ReconstructionsConfig.thumb_connections = _config["reconstruction"]["thumb_connections"]
+ReconstructionsConfig.index_connections = _config["reconstruction"]["thumb_connections"]
+ReconstructionsConfig.middle_connections = _config["reconstruction"]["index_connections"]
+ReconstructionsConfig.ring_connections = _config["reconstruction"]["middle_connections"]
+ReconstructionsConfig.pinkie_connections = _config["reconstruction"]["ring_connections"]
+ReconstructionsConfig.fingers_colors = _config["reconstruction"]["pinkie_connections"]
+
+CameraConfig.ids = _config["camera"]["ids"]
+
+FingersID.wrist = _config["fingers"]["wrist"]
+FingersID.thumb = _config["fingers"]["thumb"]
+FingersID.index = _config["fingers"]["index"]
+FingersID.middle = _config["fingers"]["middle"]
+FingersID.ring = _config["fingers"]["ring"]
+FingersID.pinky = _config["fingers"]["pinky"]
